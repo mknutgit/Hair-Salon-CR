edcb71b8d68c7ac92e1a990cbca0d20ec12a141b
matt knutson <mknut001@gmail.com>
2016-03-02 14:35:59 -0800

    can update client details

--- app/app.php
+++ app/app.php
@@ -26,14 +26,14 @@
        $stylist = new Stylist($_POST['name']);
        $stylist->save();
        return $app['twig']->render('index.html.twig', array('stylists'=> Stylist::getAll()));
-   });
+    });
 
    /**shows single stylist**/
    $app->get("/stylists/{id}", function($id) use ($app){
        $this_stylist = Stylist::find($id);
        $clients = $this_stylist->getClients();
        return $app['twig']->render('stylists.html.twig', array('clients' => $clients, 'stylist' => $this_stylist));
-   });
+    });
 
    /* add clients */
     $app->post("/clients/{id}", function($id) use ($app) {
@@ -46,6 +46,35 @@
         $stylist = Stylist::find($id);
         return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
     });
+
+    /*get Stylist by id*/
+   $app->get("/stylists/{id}", function($id) use ($app){
+       $stylist = Stylist::find($id);
+       return $app['twig']->render('stylists.html.twig', array('stylist'=> $stylist, 'clients' => $stylist->getClients()));
+    });
+
+    /// Client ///
+    /**lists out all clients page & connects to stylists**/
+    $app->get("/clients", function() use ($app) {
+       return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll(), 'stylists' => Client::getAll()));
+    });
+
+    /**find a client**/
+    $app->get("/clients/{id}/description", function($id) use ($app) {
+        $client = Client::find($id);
+        return $app['twig']->render('client_details.html.twig', array('client' => $client));
+    });
+
+    /*edit restaurant by id*/
+    $app->patch("/clients/{id}/edit", function($id) use ($app) {
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $client = Client::find($id);
+        $client->update($name, $phone, $email);
+        return $app['twig']->render('client_details.html.twig', array('client' => $client));
+    });
+
 
 
 
